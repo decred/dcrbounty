023a18bbbf1e02e2d1aeeff1d08684bb67281a6f
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-01-07 17:10:40 -0200

    add docs folder

---
+++ docs/js/bundle.min.f242c68f9b676aaeb464b5179e0d46013015f50e2f1e332dc5eeec61737f688c.js
@@ -0,0 +1,3 @@
+function test01(){console.log("test01");}
+function test02(){console.log("test02");}
+window.addEventListener('DOMContentLoaded',()=>console.log("Final load!"));
---
+++ docs/js/critical.min.09933981228eefdedea0ce57a17f9f8c37b11926a4ecfb15317dbf98c59f98df.js
@@ -0,0 +1,1 @@
+window.addEventListener('load',()=>console.log("Initial load!"));
